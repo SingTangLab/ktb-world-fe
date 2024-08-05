dda94da72b60c9cffa772e7176fb82537575474a
sen2y <pse6704@sookmyung.ac.kr>
2024-08-05 22:55:50 +0900

    ✨ 헤더 구현

---
+++ src/components/Header.jsx
@@ -0,0 +1,50 @@
+import styled from 'styled-components'
+
+export function Header() {
+  const nickname = 'seny.park'
+
+  return (
+    <HeaderContainer>
+      <Title>KTB WORLD</Title>
+      <Profile>
+        <Circle>{nickname[0]}</Circle>
+        <NickName>{nickname}</NickName>
+      </Profile>
+    </HeaderContainer>
+  )
+}
+
+const HeaderContainer = styled.header`
+  height: 55px;
+  padding: 18px 16px;
+  box-sizing: border-box;
+  font-weight: 600;
+  border-bottom: 1px solid #ccc;
+  display: flex;
+  justify-content: space-between;
+`
+
+const Title = styled.h1``
+const Profile = styled.div`
+  display: flex;
+  align-items: center;
+`
+
+const Circle = styled.div`
+  width: 30px;
+  height: 30px;
+  border-radius: 50%;
+  background-color: #6d7992;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  font-size: 12px;
+  font-weight: 600;
+  color: #fff;
+`
+
+const NickName = styled.div`
+  margin-left: 8px;
+  font-size: 12px;
+  font-weight: 500;
+`
--- src/index.js
+++ src/index.js
@@ -1,6 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
+import { Header } from './components/Header'
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
-root.render(<App />)
+root.render(
+  <>
+    <Header />
+    <App />
+  </>
+)

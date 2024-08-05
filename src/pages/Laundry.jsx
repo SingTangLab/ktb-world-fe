9d5ed2352fdf911349cf7b5dcd253e04660c43f1
erica0321 <hyunahrho29@gmail.com>
2024-08-06 01:24:08 +0900

    feature: 티켓 컴포넌트 추기

--- src/pages/Laundry.jsx
+++ src/pages/Laundry.jsx
@@ -1,9 +1,15 @@
 import styled from 'styled-components'
+import Ticket from '../components/Ticket'
 
 export function Laundry() {
+  const tickets = Array.from({ length: 10 }) // Creates an array with 10 undefined elements
+
   return (
     <LaundryContainer>
       <LaundryTitle>Laundry</LaundryTitle>
+      {tickets.map((_, index) => (
+        <Ticket key={index} />
+      ))}
     </LaundryContainer>
   )
 }

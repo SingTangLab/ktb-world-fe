d0a7629275e2852a51164f9f59062f6c4b5bcb05
sen2y <pse6704@sookmyung.ac.kr>
2024-08-06 18:42:39 +0900

    💄 티켓 컴포넌트 css update

--- src/components/Ticket.jsx
+++ src/components/Ticket.jsx
@@ -24,25 +24,43 @@ export default function Ticket() {
       <div className={styles.main}>
         <div className={styles.top}>
           <div className={styles.left}>
-            <div className={styles.write}>
+            {/* <div className={styles.write}>
               <div className={styles.writer}>erica</div>
               <div className={styles.writeTime}>07:05</div>
-            </div>
+            </div> */}
             <div className={styles.title}>흰 빨래 1명 급구!!!!!</div>
-            <div className={styles.description}>5장 이내만 가능합니다~~~~</div>
+            <div className={styles.description}>5장 이내만 가능합니다~~~</div>
             <div className={styles.info}>
-              <div className={styles.color}>색깔: 어두운 색, 밝은 색</div>
+              <div className={styles.color}>색상: 어두운 색, 밝은 색</div>
               <div className={styles.dryer}>건조기: O</div>
+              <div className={styles.write}>
+                <div className={styles.writer}>모집자 : erica</div>
+                <div className={styles.writeTime}>작성 : 07:05</div>
+              </div>
             </div>
+            {isLaundry && (
+              <div className={styles.bottom}>
+                <div className={styles.bottomLeft}>
+                  <div className={styles.start}>시작 : 07:40</div>
+                  <div className={styles.end}>종료 : 08:20</div>
+                </div>
+                {/* <div className={styles.bottomRight}>빨래중</div> */}
+              </div>
+            )}
+            {isTaxi && (
+              <div className={styles.bottom}>
+                <div className={styles.start}>출발: 07:40</div>
+              </div>
+            )}
           </div>
           <div className={styles.right}>
-            <div className={styles.people}>3/4</div>
+            <div className={styles.people}>3 / 4</div>
             <div classnane={styles.status}>
               <img src={check} className={styles.statusImage} />
             </div>
           </div>
         </div>
-        {isLaundry && (
+        {/* {isLaundry && (
           <div className={styles.bottom}>
             <div className={styles.bottomLeft}>
               <div className={styles.start}>시작: 07:40</div>
@@ -55,7 +73,7 @@ export default function Ticket() {
           <div className={styles.bottom}>
             <div className={styles.start}>출발: 07:40</div>
           </div>
-        )}
+        )} */}
       </div>
     </div>
   )
